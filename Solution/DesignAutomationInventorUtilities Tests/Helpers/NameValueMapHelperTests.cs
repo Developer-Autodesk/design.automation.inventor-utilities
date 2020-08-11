073e0c386558b015473f29ed743e4070df59f408
Dc41f <jl.dc41f@gmail.com>
2020-08-11 14:13:39 +0200

    New utility class for working with the improved NameValueMap functionality added. Unit tests added to the project.

---
+++ Solution/DesignAutomationInventorUtilities Tests/Helpers/NameValueMapHelperTests.cs
@@ -0,0 +1,92 @@
+﻿using Autodesk.Forge.DesignAutomation.Inventor.Utils.Helpers;
+using Inventor;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace DesignAutomationInventorUtilities_Tests.Helpers
+{
+    public class NameValueMapHelperTests
+    {
+        class TestClass
+        {
+
+        }
+
+        enum TestEnum
+        {
+            VALUE_ONE = 10,
+            VALUe_TWO, 
+            VALUE_THREE,
+            VALUE_FOUR
+        }
+
+        [Fact]
+        public void Test1()
+        {
+            NameValueMap nameValueMap = new NameValueMapStub();
+            nameValueMap.Value["Test"] = "TestString";
+            nameValueMap.Value["Test2"] = new TestClass();
+
+            NameValueMapHelper nameValueMapHelper = new NameValueMapHelper(nameValueMap);
+
+            /*
+            ///////////
+            NameValueMapValue value = new NameValueMapValue("Test");
+            NameValueMapValue value2 = new NameValueMapValue(1);
+            NameValueMapValue value3 = new NameValueMapValue(new object[]{ "Ziltoid", true, 3});
+
+            Console.Write(value.AsString);
+            Console.Write(value2.AsInt);
+            int v = value3.AsCollection()[0].AsInt;
+            Console.Write(value3.AsCollection()[0]);
+            */
+
+            // Collection example
+            NameValueMapValue collectionValue = new NameValueMapValue("value_one value_two value_three value_four");
+            var list1 = collectionValue.AsCollection();
+
+            NameValueMapValue numericalArray = new NameValueMapValue("1,2,3,4,5,6,7,8,9,10");
+            var list2 = numericalArray.AsCollection(',');
+
+            // Enum Example
+            NameValueMapValue enumValue = new NameValueMapValue("value_one");
+            TestEnum e1 = enumValue.AsEnum<TestEnum>();
+
+            // String to int and str
+            NameValueMapValue strIntValue = new NameValueMapValue("196");
+            bool s1 = strIntValue.TryGetAs<int>(out int outValue1);
+            bool s2 = strIntValue.TryGetAs<string>(out string outValueStr1);
+            Assert.Equal(196, outValue1);
+
+            // Int to string and int
+            NameValueMapValue intValue = new NameValueMapValue("2885");
+            s1 = intValue.TryGetAs<int>(out int outValue2);
+            s2 = intValue.TryGetAs<string>(out string outValueStr2);
+            bool s3 = intValue.TryGetAs<double>(out double outValueFloat);
+            Assert.Equal(2885, outValue2);
+
+            // Floating test
+            NameValueMapValue floatingValue = new NameValueMapValue("739.7889");
+            s1 = floatingValue.TryGetAs<int>(out int outValue3);
+            s2 = floatingValue.TryGetAs<string>(out string outValueStr3);
+            s3 = floatingValue.TryGetAs<double>(out double outValueDouble);
+            Assert.Equal(739.7889d, outValueDouble);
+
+            // Bool test
+            NameValueMapValue boolValue = new NameValueMapValue("true");
+            s1 = boolValue.TryGetAs<bool>(out bool outBoolValue1);
+            s2 = boolValue.TryGetAs<string>(out string outValueStr4);
+            s3 = boolValue.TryGetAs<int>(out int outValue4);
+            Assert.True(outBoolValue1);
+
+            NameValueMapValue boolValue2 = new NameValueMapValue("1");
+            s1 = boolValue2.TryGetAs<bool>(out bool outBoolValue2);
+            s2 = boolValue2.TryGetAs<string>(out string outValueStr5);
+            s3 = boolValue2.TryGetAs<int>(out int outValue5);
+
+            Assert.False(outBoolValue2);
+        }
+    }
+}
---
+++ Solution/DesignAutomationInventorUtilities/Helpers/NameValueMapHelper.cs
@@ -0,0 +1,170 @@
+﻿/////////////////////////////////////////////////////////////////////
+// Copyright (c) Autodesk, Inc. All rights reserved
+// Written by Forge Partner Development
+//
+// Permission to use, copy, modify, and distribute this software in
+// object code form for any purpose and without fee is hereby granted,
+// provided that the above copyright notice appears in all copies and
+// that both that copyright notice and the limited warranty and
+// restricted rights notice below appear in all supporting
+// documentation.
+//
+// AUTODESK PROVIDES THIS PROGRAM "AS IS" AND WITH ALL FAULTS.
+// AUTODESK SPECIFICALLY DISCLAIMS ANY IMPLIED WARRANTY OF
+// MERCHANTABILITY OR FITNESS FOR A PARTICULAR USE.  AUTODESK, INC.
+// DOES NOT WARRANT THAT THE OPERATION OF THE PROGRAM WILL BE
+// UNINTERRUPTED OR ERROR FREE.
+/////////////////////////////////////////////////////////////////////
+
+using Inventor;
+using System;
+using System.Collections.Generic;
+
+namespace Autodesk.Forge.DesignAutomation.Inventor.Utils.Helpers
+{
+    public class InvalidValueTypeException : Exception
+    {
+        public InvalidValueTypeException(string message) : base(message)
+        { }
+    }
+
+    public class NameValueMapValue
+    {
+        private readonly object value;
+
+        public string AsString
+        {
+            get
+            {
+                if (TryGetAs<string>(out string strValue))
+                    return strValue;
+
+                throw new InvalidValueTypeException("Value cannot be used as a string");
+            }
+        }
+
+        public int AsInt
+        {
+            get
+            {
+                if (TryGetAs<int>(out int intValue))
+                    return intValue;
+
+                throw new InvalidValueTypeException("Value cannot be used as an integer");
+            }
+        }
+
+        public double AsDouble
+        {
+            get
+            {
+                if (TryGetAs<double>(out double doubleValue))
+                    return doubleValue;
+
+                throw new InvalidValueTypeException("Value cannot be used as a double");
+            }
+        }
+
+        public bool AsBool
+        {
+            get
+            {
+                if (TryGetAs<bool>(out bool boolValue))
+                    return boolValue;
+
+                throw new InvalidValueTypeException("Value cannot be used as a boolean");
+            }
+        }
+
+        public List<NameValueMapValue> AsCollection(char separator = ' ')
+        {
+            List<NameValueMapValue> collection = null;
+
+            if (value is string stringValue)
+            {
+                collection = new List<NameValueMapValue>();
+                string[] splitString = stringValue.Split(separator);
+                foreach (string str in splitString)
+                    collection.Add(new NameValueMapValue(str));
+            }
+            else if (value is System.Collections.IEnumerable enumerableValue)
+            {
+                collection = new List<NameValueMapValue>();
+                foreach (object value in enumerableValue)
+                    collection.Add(new NameValueMapValue(value));
+            }
+
+            if (collection == null)
+                throw new InvalidValueTypeException("Value cannot be used as a collection");
+
+            return collection;
+        }
+
+        public T AsEnum<T>(bool ignoreCase = true) where T: struct, IConvertible
+        {
+            if (!typeof(T).IsEnum)
+            {
+                throw new ArgumentException("T is not an enum type");
+            }
+
+            T enumValue;
+
+            try
+            {
+                enumValue = (T)Enum.Parse(typeof(T), value.ToString(), ignoreCase);
+            }
+            catch (Exception) 
+            {
+                throw new InvalidValueTypeException("Value cannot be used as an enum");
+            }
+
+            return enumValue;
+        }
+
+        public NameValueMapValue(object value)
+        {
+            this.value = value;
+        }
+
+        public bool TryGetAs<T>(out T outValue)
+        {
+            if (value is T convertedValue)
+            {
+                outValue = convertedValue;
+                return true;
+            }
+
+            bool success;
+
+            try
+            {
+                outValue = (T)Convert.ChangeType(value, typeof(T));
+                success = true;
+            }
+            catch (Exception)
+            {
+                outValue = default(T);
+                success = false;
+            }
+
+            return success;
+        }
+    }
+
+    public class NameValueMapHelper
+    {
+        private readonly NameValueMap nameValueMap;
+
+        public NameValueMapValue this[string index]
+        {
+            get { return new NameValueMapValue(nameValueMap.Value[index]); }
+        }
+
+        public NameValueMapHelper(NameValueMap nameValueMap)
+        {
+            this.nameValueMap = nameValueMap;
+        }
+
+        public bool HasKey(string key) => nameValueMap.Value[key] != null;
+    }
+}
